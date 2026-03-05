1db28e71cd98b33874be293843457cf3c5734453
GajananKKale <gajanankkale1998.gk@gmail.com>
2026-03-05 16:51:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv'
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+let app = express()
+import { router } from "./routes/routes.js"
+import path from "path"
+dotenv.config()
+
+let PORT = process.env.port || 8000
+
+let _dirname = path.resolve()
+
+//middleware
+app.use(cors({
+    origin: "http://localhost:5173",
+    // origin: "https://userregistration-1-a0md.onrender.com",
+    credentials: true
+}));
+
+app.use(express.json())
+
+app.use("/api", router)
+
+
+
+app.listen(PORT, () => {
+    mongoose.connect(process.env.mongo_url).then(() => {
+        console.log("connected to Database");
+    }).catch((error) => {
+        (
+            console.log(error)
+        )
+    })
+    console.log(`server is running on port : ${PORT}`);
+})
